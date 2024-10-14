6902f78ad794954548cc07658ca62483dfc00ed5
yaroslav.alelekov <yaroslav.alelekov@gmail.com>
2024-10-14 14:09:48 +0700

    Десятая и одиннадцатая, девятую случайно редактировал

--- task10.py
+++ task10.py
@@ -3,6 +3,7 @@ y_first = int(input('y_first >> '))
 x_second = int(input('x_second >> '))
 y_second = int(input('y_second >> '))
 
+
 if x_second > x_first and abs(y_second - y_first) == x_second - x_first:
     print('YES')
 elif x_second < x_first and abs(y_second - y_first) == x_first - x_second:
